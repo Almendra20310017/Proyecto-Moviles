5819e4217c75a37c3dedf162a5f34ddb80a34df7
code-504 <ruben.v.p@live.com>
2022-12-02 21:15:01 -0600

    Pantalla de inicio

--- app/src/main/java/com/example/proyecto_moviles/MainActivity.java
+++ app/src/main/java/com/example/proyecto_moviles/MainActivity.java
@@ -1,16 +1,31 @@
 package com.example.proyecto_moviles;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        // hola
+        Window window = this.getWindow();
+
+// clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.bluebd));
+
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
     }
 }
