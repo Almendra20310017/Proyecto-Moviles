429756ed5bd01a066376af88b8ac791caf243f74
code-504 <ruben.v.p@live.com>
2022-12-02 21:18:56 -0600

    Pantalla de inicio

--- app/src/main/java/com/example/proyecto_moviles/MainActivity.java
+++ app/src/main/java/com/example/proyecto_moviles/MainActivity.java
@@ -14,15 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         Window window = this.getWindow();
-
-// clear FLAG_TRANSLUCENT_STATUS flag:
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
-// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-
-// finally change the color
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.bluebd));
+        this.getWindow().setNavigationBarColor(getResources().getColor(R.color.bluebd));
 
         getSupportActionBar().hide();
 
